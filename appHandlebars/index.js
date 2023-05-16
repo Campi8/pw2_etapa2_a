2fd7d3a7508bfa29c455fda9a26cf3312f803388
Campi8 <gustavocampi02@gmail.com>
2023-05-15 22:46:17 -0300

    Aula 1505

--- appHandlebars/index.js
+++ appHandlebars/index.js
@@ -6,14 +6,37 @@ const app = express()
 
 app.engine('handlebars', exphbs())
 app.set('view engine','handlebars')
+const approved = false;
+
+app.post('/verifyuser', (req, res) =>{
+    const form ={
+        nome:req.body.name,
+        idade:req.body.age
+    }
+
+    if(form.nome =='lindo' && form.age == 22){
+        approved = true;
+
+    }else{
+        approved = false;
+    }
+    res.send(approved)
+})
+
 
 app.get('/', function(req, res){
     const user ={
-        name: "Victor",
-        surname: "Icoma",
-        age: 41
+        name: "Gustavo",
+        surname: "Campi",
+        age: 22
     }
-    res.render('home', {user:user})
+
+
+    res.render('home', {user:user, auth:approved, approved})
+})
+
+app.get('/dashboards', function(req, res){
+    res.render('dashboards')
 })
 
 app.listen(3000)
